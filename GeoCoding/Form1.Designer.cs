4f53d2718ead72dce85745c4b11431829a18ab45
mehmeddogan <mehmetdogan5324@gmail.com>
2021-03-29 11:00:55 +0300

    first commit

---
+++ GeoCoding/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GeoCoding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_Enlem = new System.Windows.Forms.TextBox();
+            this.txt_Boylam = new System.Windows.Forms.TextBox();
+            this.btn_Find = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Enlem
+            // 
+            this.txt_Enlem.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_Enlem.ForeColor = System.Drawing.Color.ForestGreen;
+            this.txt_Enlem.Location = new System.Drawing.Point(329, 25);
+            this.txt_Enlem.Name = "txt_Enlem";
+            this.txt_Enlem.Size = new System.Drawing.Size(100, 20);
+            this.txt_Enlem.TabIndex = 0;
+            // 
+            // txt_Boylam
+            // 
+            this.txt_Boylam.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_Boylam.ForeColor = System.Drawing.Color.ForestGreen;
+            this.txt_Boylam.Location = new System.Drawing.Point(329, 64);
+            this.txt_Boylam.Name = "txt_Boylam";
+            this.txt_Boylam.Size = new System.Drawing.Size(100, 20);
+            this.txt_Boylam.TabIndex = 1;
+            // 
+            // btn_Find
+            // 
+            this.btn_Find.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btn_Find.Location = new System.Drawing.Point(465, 44);
+            this.btn_Find.Name = "btn_Find";
+            this.btn_Find.Size = new System.Drawing.Size(75, 23);
+            this.btn_Find.TabIndex = 2;
+            this.btn_Find.Text = "Find";
+            this.btn_Find.UseVisualStyleBackColor = true;
+            this.btn_Find.Click += new System.EventHandler(this.btn_Find_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(287, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enlem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(282, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Boylam";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.Location = new System.Drawing.Point(92, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(603, 205);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Find);
+            this.Controls.Add(this.txt_Boylam);
+            this.Controls.Add(this.txt_Enlem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Enlem;
+        private System.Windows.Forms.TextBox txt_Boylam;
+        private System.Windows.Forms.Button btn_Find;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ GeoCoding/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Net.Http;
+using System.Net;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace GeoCoding
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+
+        public Form1()
+        {
+            InitializeComponent();
+            baglanti = new SqlConnection("Data Source=MEMMED\\SQLEXPRESS;Initial Catalog=GeoCoding;Integrated Security=True");
+        }
+        void AdresGetir()
+        {
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT * FROM Address", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AdresGetir();
+           
+        }
+        void Coordinate(string enlem, string boylam)
+        {
+            baglanti.Open();
+            string sorgu = "INSERT INTO Coordinate(Enlem,Boylam) VALUES (@Enlem,@Boylam)";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Enlem", enlem);
+            komut.Parameters.AddWithValue("@Boylam", boylam);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+        }
+        void CoordinateSil(String enlem, String boylam)
+        {
+            string sorgu = "DELETE FROM Coordinate WHERE @Enlem=Enlem AND @Boylam=Boylam";
+            komut = new SqlCommand(sorgu,baglanti);
+            komut.Parameters.AddWithValue("@Enlem", enlem);
+            komut.Parameters.AddWithValue("@Boylam", boylam);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            AdresGetir();
+        }
+        private async void btn_Find_Click(object sender, EventArgs e)
+        {
+            if (txt_Enlem.Text == "" || txt_Boylam.Text == "")
+            {
+                MessageBox.Show("Bos alan bırakmayınız");
+            }
+            else
+            {
+                double Enlem, Boylam;
+                Enlem = Convert.ToDouble(txt_Enlem.Text);
+                Boylam = Convert.ToDouble(txt_Boylam.Text);
+                Coordinate(txt_Enlem.Text, txt_Boylam.Text);
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://maps.googleapis.com/");
+                HttpResponseMessage response = await client.GetAsync("maps/api/geocode/xml?latlng=" + txt_Enlem.Text + "," + txt_Boylam.Text + "&key=AIzaSyADd2ntBd2tlefA5W9BYb4ymnZWSuG2iWU");
+                String result = await response.Content.ReadAsStringAsync();
+                List<String> addresses = new List<String>();
+
+                using (StreamReader reader = new StreamReader(await response.Content.ReadAsStreamAsync(), Encoding.UTF8))
+                {
+                    DataSet dsResult = new DataSet();
+                    dsResult.ReadXml(reader);
+                    try
+                    {
+                        foreach (DataRow row in dsResult.Tables["result"].Rows)
+                        {
+                            string fullAddress = row["formatted_address"].ToString();
+                            //Console.WriteLine(fullAddress);
+                            addresses.Add(fullAddress);
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                }
+                if (addresses.Count != 0)
+                {
+                    baglanti.Open();
+                    foreach (String address in addresses)
+                    {
+                        string sorgu = "INSERT INTO Address(Enlem,Boylam,Adres) VALUES (@Enlem,@Boylam,@Adres)";
+                        komut = new SqlCommand(sorgu, baglanti);
+                        komut.Parameters.AddWithValue("@Enlem", txt_Enlem.Text);
+                        komut.Parameters.AddWithValue("@Boylam", txt_Boylam.Text);
+                        komut.Parameters.AddWithValue("@Adres", address);
+                        komut.ExecuteNonQuery();
+                    }
+                    baglanti.Close();
+                    AdresGetir();
+                    CoordinateSil(txt_Enlem.Text, txt_Boylam.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Koordinat Verileri Hatalı.En fazla ikinci rakamdan sonra nokta koyulmalı ve noktadan sonra en fazla 6 tane deger girilmelidir!!!\n Örn:\n Enlem=XX.XXXXXX\n Boylam=XX.XXXXXX");
+                    CoordinateSil(txt_Enlem.Text, txt_Boylam.Text);
+                }
+            }
+        }
+    }
+    
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+    
+
+}
